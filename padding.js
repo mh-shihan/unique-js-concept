64e325358a50e081015375d36807a314d0eb87cf
MHShihan <mahmodul.no.1@gmail.com>
2025-02-11 21:19:04 +0600

    Make some change in paddin.js

--- padding.js
+++ padding.js
@@ -1,10 +1,10 @@
 //  This concept use to constant an id
 // 2221081098
 
-let id = "2221081001";
+let id = 2221081001;
 
 for (let i = 0; i < 140; i++) {
-  const intId = parseInt(id) + i;
-  const afterPadding = intId.toString().padStart(10, "0");
+  const newId = id + i;
+  const afterPadding = newId.toString().padStart(10, "0");
   console.log(afterPadding);
 }
